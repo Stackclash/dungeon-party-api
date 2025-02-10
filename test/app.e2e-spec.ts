44f4257b1147f8dada4a15a59bfc75c76b4eedf5
Richard Cox <cox.richard200@gmail.com>
2025-02-10 10:10:52 -0600

    updating e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,10 +17,7 @@ describe('AppController (e2e)', () => {
     await app.init()
   })
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!')
+  it('PLACEHOLDER', () => {
+    return request(app.getHttpServer()).get('/').expect(404)
   })
 })
